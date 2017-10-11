5e69456c1a6b194a1c1dd2f816a774fe22eb4a4c
rdundon <rmdundon@gmail.com>
2017-10-11 15:05:54 -0400

    Some changes

--- convert_to_csv.php
+++ convert_to_csv.php
@@ -1,21 +1,24 @@
 <?php
-
-function convert_to_csv($input_array, $output_file_name, $delimiter)
+// Adapted from http://webtricksandtreats.com/export-to-csv-php/
+// But what if we want to use the CSV data other than just writing to a file?
+function convert_to_csv($input_array, $delimiter=",", $enclosure='"')
 {
     /** open raw memory as file, no need for temp files, be careful not to run out of memory thought */
     $f = fopen('php://memory', 'w');
     /** loop through array  */
     foreach ($input_array as $line) {
         /** default php csv handler **/
-        fputcsv($f, $line, $delimiter);
+        fputcsv($f, $line, $delimiter, $enclosure);
     }
     /** rewrind the "file" with the csv lines **/
     fseek($f, 0);
-    /** modify header to be downloadable csv file **/
-    header('Content-Type: application/csv');
-    header('Content-Disposition: attachement; filename="' . $output_file_name . '";');
-    /** Send file to browser for download */
-    fpassthru($f);
+
+	$csv_output = '';
+    while (!feof($f)) {
+		$csv_output .= fgets($f);
+	}
+	fclose($f);
+	return $csv_output;
 }
 
 /** Array to convert to csv */
@@ -36,4 +39,10 @@ $array_to_csv = Array(
 
 );
 
-convert_to_csv($array_to_csv, 'report.csv', ',');
+/**
+ * Outputs to:
+ * 12566,Enmanuel,Corvo
+ * 56544,John,Doe
+ * 78550,Mark,Smith
+ */
+echo convert_to_csv($array_to_csv);
